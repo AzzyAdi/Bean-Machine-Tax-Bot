a4ed2614246a4e1dff912e9b4e76f0af7e99a295
AzzyAdi <thegamemode12@gmail.com>
2026-06-14 19:05:21 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -288,4 +288,10 @@ async def forceremind(interaction: discord.Interaction, member: discord.Member):
     await interaction.response.send_message("Sent", ephemeral=True)
 
 # ---------------- RUN ----------------
+# ---------------- RUN ----------------
+print("TOKEN EXISTS:", TOKEN is not None)
+print("TOKEN LENGTH:", len(TOKEN) if TOKEN else 0)
+print("TOKEN STARTS WITH:", TOKEN[:5] if TOKEN else "NONE")
+
+bot.run(TOKEN)
 bot.run(TOKEN)
